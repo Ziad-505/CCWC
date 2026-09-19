fdd75bff3e18d5f9efb3db182971d16ba073d7dc
Ziad Ahmed Abdelhady <ziaddahmed177@gmail.com>
2026-09-19 18:15:43 +0300

    Add default output and edge-case fixtures

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,40 @@ import { argv, stderr } from 'node:process';
 import { getFileSize, getFileInfo, getLineCount, getWordCount, getCharacterCount } from './file.js';
 
 try {
-    if(argv.length !== 4) {
-        stderr.write('Usage: ccwc -<option> <file>\n');
+    if(argv.length < 3 || argv.length > 4) {
+        stderr.write('Usage: ccwc [-<option>] <file>\n');
         process.exit(1);
     }
-
-    const option = argv[2];
-    const fileName = argv[3];
-
-    if(option === '-c') {
-        const fileInfo = getFileInfo(fileName)
-        console.log(`${getFileSize(fileInfo)} ${fileName}`);
-    }else if(option === '-l'){
-        console.log(`${getLineCount(fileName)} ${fileName}`);  
-    }else if(option === '-w'){
-        console.log(`${getWordCount(fileName)} ${fileName}`);  
-    }else if(option === '-m'){
-        console.log(`${getCharacterCount(fileName)} ${fileName}`);  
+    if(argv.length === 3){
+        const fileName = argv[2];
+        if(fileName.startsWith("-")) {
+            stderr.write('Usage: ccwc [-<option>] <file>\n');
+            process.exit(1);
+        }
+        const fileInfo = getFileInfo(fileName);
+        console.log(`${getLineCount(fileName)} ${getWordCount(fileName)} ${getFileSize(fileInfo)} ${fileName}`);
     }else {
-        stderr.write(`Error: ${option} is not a supported option\n`);
-        process.exit(1);    
+        const option = argv[2];
+        const fileName = argv[3];
+        if(option === '-c') {
+            const fileInfo = getFileInfo(fileName);
+            console.log(`${getFileSize(fileInfo)} ${fileName}`);
+        }else if(option === '-l'){
+            console.log(`${getLineCount(fileName)} ${fileName}`);
+        }else if(option === '-w'){
+            console.log(`${getWordCount(fileName)} ${fileName}`);
+        }else if(option === '-m'){
+            console.log(`${getCharacterCount(fileName)} ${fileName}`);
+        }else {
+            stderr.write(`Error: ${option} is not a supported option\n`);
+            process.exit(1);
+        }
     }
-
 } catch (error) {
-
     if(error instanceof Error) {
         stderr.write("Error: " + error.message + '\n');
     }else {
         stderr.write('Error: Unknown Error occurred\n');
     }
-
     process.exit(1);
 }
